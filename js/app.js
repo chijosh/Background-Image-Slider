5dbfc4937d2f9825250fd7c7c09c0074a6e99207
Chi Joshua <37910718+chijosh@users.noreply.github.com>
2019-07-12 12:46:50 +0300

    js img slider logic

--- js/app.js
+++ js/app.js
@@ -6,6 +6,50 @@
     "contBcg-1",
     "contBcg-2",
     "contBcg-3",
-    "contBcg-4"
+    "contBcg-4",
+    "contBcg-5",
+    "contBcg-6"
   ];
+
+  // setup counter
+
+  let counter = 0;
+
+  // select both the buttons
+
+  const buttons = document.querySelectorAll(".btn");
+  // console.log(buttons);
+
+  //loop through the nodelist and attach the event listeners
+
+  buttons.forEach(function(btn) {
+    btn.addEventListener("click", function(event) {
+      //get event target
+      const clickBtn = event.target;
+
+      // console.log(check);
+      //if condition to change image for left button
+      if (clickBtn.classList.contains("btn-left")) {
+        counter--;
+      }
+      if (counter < 0) {
+        counter = pictures.length - 1;
+      }
+      document.querySelector(
+        ".img-container"
+      ).style.backgroundImage = `url('img/${pictures[counter]}.jpg')`;
+
+      //if condition to change image for right button
+      //
+      if (clickBtn.classList.contains("btn-right")) {
+        counter++;
+      }
+      if (counter > pictures.length - 1) {
+        counter = 0;
+      }
+      document.querySelector(
+        ".img-container"
+      ).style.backgroundImage = `url('img/${pictures[counter]}.jpg')`;
+    });
+  });
 })();
